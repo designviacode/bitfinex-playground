a9d5502fa0ac3fe852a2cd69caae18c7c10fb4a6
Manpreet Singh <designviacode@gmail.com>
2018-10-17 14:33:55 +0530

    initial setup

---
+++ src/containers/App/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Let's play with the Bitfinex sockets API</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
